095138c41433942668d13b050739298e5739c731
Emilce <emilceagustinatorres@gmail.com>
2025-06-08 20:53:34 -0300

    Implementa función para eliminar usuarios

--- programacion/src/gestor_usuario.py
+++ programacion/src/gestor_usuario.py
@@ -54,46 +54,11 @@ class GestorUsuarios:
         print(f"No se encontró usuario con nombre '{nombre_usuario}'.")
         return False
     
-            # visualizacion de informacion del usuario
-class VisualizacionDeInformacionDelUsuario(registro):
-    def __init__(self, usuario):
-        self.usuario = usuario
-        
-    def mostrar_informacion(self):
-        print(f"Nombre: {self.usuario.nombre}")
-        print(f"Apellido: {self.usuario.apellido}")
-        print(f"Edad: {self.usuario.edad}")
-        print(f"Email: {self.usuario.email}")
-
-datos_usuario = VisualizacionDeInformacionDelUsuario(persona)
-datos_usuario.mostrar_informacion()
-
-# Actualizacion de informacion del usuario
-class ActualizacionDeInformacionDelUsuario(registro):
-    def __init__(self, usuario):
-        self.usuario = usuario
-        
-    def actualizar_informacion(self):
-        self.usuario.nombre = input("Ingrese su nuevo nombre: ")
-        self.usuario.apellido = input("Ingrese su nuevo apellido: ")
-        self.usuario.edad = int(input("Ingrese su nueva edad: "))
-        self.usuario.email = input("Ingrese su nuevo email: ")
-        self.usuario.contraseña = input("Ingrese su nueva contraseña: ")
-        print("Sus datos han sido actualizados correctamente")
-datos_actualizados = ActualizacionDeInformacionDelUsuario(persona)
-datos_actualizados.actualizar_informacion()
-
-# Eliminacion de cuenta del usuario
-class EliminacionDeCuenta(registro):
-    def __init__(self, usuario):
-        self.usuario = usuario
-        
-    def eliminar_cuenta(self):
-        confirmacion = input("¿Está seguro de que desea eliminar su cuenta? (si/no): ")
-        if confirmacion.lower() == 'si':
-            print("Su cuenta ha sido eliminada.")
-            self.usuario = None  # Elimina la referencia al usuario
-        else:
-            print("La cuenta no ha sido eliminada.")
-datos_eliminacion = EliminacionDeCuenta(persona)
-datos_eliminacion.eliminar_cuenta()
+    def eliminar_usuario(self, nombre_usuario):
+        for usuario in self.usuarios:
+            if usuario.nombre_usuario == nombre_usuario:
+                self.usuarios.remove(usuario)
+                print(f"Usuario '{nombre_usuario}' eliminado.")
+                return True
+        print(f"No se encontró usuario con nombre '{nombre_usuario}'.")
+        return False
