a80683c67fbb5709454d9f38bd725b7d6932c322
Gerardo <gerardo@catalasjuin.com>
2025-06-08 22:30:56 -0300

    se actualiza la parte de base de datos

--- base_de_datos/scripts/01_creacion_db.sql
+++ base_de_datos/scripts/01_creacion_db.sql
@@ -1,20 +1,24 @@
--- Script para crear las tablas
+-- Crear base de datos
+CREATE DATABASE IF NOT EXISTS planificador_usuarios;
+USE planificador_usuarios;
 
--- Tabla Roles
-CREATE TABLE Roles (
+-- Crear tabla de roles
+CREATE TABLE roles (
     id_rol INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_rol VARCHAR(50) NOT NULL UNIQUE
+    nombre VARCHAR(50) NOT NULL
+);
+
+-- Crear tabla de usuarios
+CREATE TABLE usuarios (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_usuario VARCHAR(50) NOT NULL UNIQUE,
+    contraseña VARCHAR(100) NOT NULL,
+    datos_personales VARCHAR(100) NOT NULL,
+    id_rol INT NOT NULL,
+    FOREIGN KEY (id_rol) REFERENCES roles(id_rol)
 );
 
 -- Insertar roles base
-INSERT INTO Roles (nombre_rol) VALUES ('ESTANDAR'), ('ADMIN');
-
--- Tabla Usuarios
-CREATE TABLE Usuarios (
-    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_usuario VARCHAR(100) NOT NULL UNIQUE,
-    contrasena VARCHAR(255) NOT NULL, -- Para almacenar hashes
-    datos_personales TEXT NULL,
-    id_rol INT NOT NULL,
-    CONSTRAINT fk_rol FOREIGN KEY (id_rol) REFERENCES Roles(id_rol)
-);
+INSERT INTO roles (nombre) VALUES 
+('Administrador'),
+('Usuario Estándar');
